a7ded936cc0bfeb5133a060fec9339aa69d9e527
LiX-u <vx_i@qq.com>
2024-07-17 21:25:30 +0800

    项目初始化

---
+++ ware/src/test/java/com/lixv/onlinemall/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lixv.onlinemall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

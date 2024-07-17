173a1dacf5f4525a3c38da9edd6a7dd10772d9ea
LiX-u <vx_i@qq.com>
2024-07-17 21:25:30 +0800

    项目初始化

---
+++ goods/src/main/java/cn/lixv/store/goods/GoodsApplication.java
@@ -0,0 +1,13 @@
+package cn.lixv.store.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class, args);
+    }
+
+}
---
+++ goods/src/test/java/cn/lixv/store/goods/GoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lixv.store.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
